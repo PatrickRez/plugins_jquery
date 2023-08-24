c1354dbc39284bb69e283ba7df7f7bc22f37e6ac
Patrick Rezende <96663359+PatrickRez@users.noreply.github.com>
2023-08-24 20:15:53 -0300

    Tarefa plugins mask
    Mask em validação
    plugin jQuery Mask Plugin para aplicar
    máscara aos campos CPF, telefone e CEP

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+    $('#telefone').mask('(00) 0 0000-0000')
+    $('#Cep').mask('0 0000-000')
+    $('#Cpf').mask('000-000-000-00')
+
+    $('formValid').validate({
+        rules: {
+            nome:{
+                required:true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+                Number:true
+            },
+            Cep: {
+                required: true,
+                Number:true
+            },
+            Cpf:{
+                required: true,
+                Number: true
+            }
+        },
+        messages:{
+            nome:'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numeberOfInvalids();
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
